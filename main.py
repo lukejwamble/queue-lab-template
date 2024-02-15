3480e58d911239c4d76cbcb78010d3eb4e2cec5b
lukejwamble <wambleson3@gmail.com>
2024-02-15 10:10:13 -0600

    fix 2
    added arguement to remove

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Queue():
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def pop(self):
-        self.cards.remove()
+        self.cards.remove(1)
 
 if __name__ == '__main__':
     #REMOVE PASS AND YOUR CODE GOES HERE
