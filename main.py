ab3004cc2feb8651184290fc8353c4fe2a39c8b9
lukejwamble <wambleson3@gmail.com>
2024-02-15 10:08:55 -0600

    fix 1
    added methods

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ class Queue():
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def push(self, card):
-        pass
+        self.cards.append(card)
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def pop(self):
-        pass
+        self.cards.remove()
 
 if __name__ == '__main__':
     #REMOVE PASS AND YOUR CODE GOES HERE
